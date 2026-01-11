f193898b3b85d8900fd41a3f8f90149df73d1014
Kris Hall <kris.hall@rechargealaska.net>
2026-01-11 07:15:17 +0000

    Redirect / to /app

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,5 @@
-// app/page.tsx
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Immediately send users to the login page when they hit the root URL
-  redirect("/login");
-
-  // This return is never actually rendered because of the redirect,
-  // but it satisfies the component contract.
-  return null;
+  redirect("/app");
 }
